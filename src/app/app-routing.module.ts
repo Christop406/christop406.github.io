301f4e0b7cc4f5d649058c103dff8b8980ebb7f2
Chris Gilardi <chris.gilardi@vidaloop.com>
2023-04-24 15:31:07 -0700

    Adds empty page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => import('./pages/home/home.page'),
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledBlocking'
-})],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {
+        useHash: true,
+        initialNavigation: 'enabledBlocking'
+    })],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'site-home',
+    templateUrl: './home.page.html',
+    standalone: true,
+})
+export default class HomePage {
+
+}
